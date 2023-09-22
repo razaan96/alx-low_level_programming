f7caa4827737182e6fe27d9424a0f8a79de3e492
Razan <razaan.alaa@hotmail.com>
2023-09-22 00:16:57 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list
+ * @h: A pointer to list head
+ * Return: The number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+		return (0);
+	for (i = 1; h->next != NULL; i++)
+	{
+		if (h->str == NULL)
+			printf("[%u] %s\n", h->len, "(nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	printf("[%u] %s\n", h->len, h->str);
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @next: pointer to the next node
+ * @len: length of string
+ * @str: string (malloced string)
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
